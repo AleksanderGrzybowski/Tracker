486bf83cb8f081e886e30729180939936e0e5fb0
Aleksander Grzybowski <kelog@orange.pl>
2016-07-25 20:09:15 +0200

    Basic project setup

---
+++ frontend/app/entry.js
@@ -0,0 +1,5 @@
+import HelloWorld from './HelloWorld.jsx';
+import ReactDOM from 'react-dom';
+import React from 'react'; // required, see https://github.com/eslint/eslint/issues/1867#issuecomment-76132915
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('content'));
